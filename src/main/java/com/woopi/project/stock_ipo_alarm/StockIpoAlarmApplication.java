8c464d2505e04aa7e3c560b186ecdd500549aadc
woopi1030 <lozenkrantz@gmail.com>
2024-05-29 00:42:14 +0900

    Init Commit

---
+++ src/main/java/com/woopi/project/stock_ipo_alarm/StockIpoAlarmApplication.java
@@ -0,0 +1,13 @@
+package com.woopi.project.stock_ipo_alarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockIpoAlarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockIpoAlarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woopi/project/stock_ipo_alarm/StockIpoAlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woopi.project.stock_ipo_alarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockIpoAlarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
